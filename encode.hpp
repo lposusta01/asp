3be62190f2278743c8b1ad97cfc078c5a97c2a04
Liz Posusta <lposusta01@m-mpx03qqk4c.admin.bvsd.k12.co.us>
2025-01-09 14:24:58 -0700

    barebones template- subject to change drastically\!

---
+++ encode.hpp
@@ -0,0 +1,16 @@
+class stream{
+
+private:
+
+double current_word;
+double next_word;
+int address;
+
+public:
+
+void double_words()
+{
+    return;
+}
+
+};
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include "encode.hpp"
+#include "decode.hpp"
+
+int main()
+{
+    return 0;
+}
